5c20d7b64f674e04832e293cd37fce3fb72df63e
rcph <rcph110110@gmail.com>
2022-11-04 13:33:24 +0800

    done

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.get("/",(req,res) => {
-    res.send(`<a href="./about"> redirect </a>`)
+    fs.readFileSync("./index.html","utf-8",(err,data) => {
+        res.send(`${data}`)
+    })
 })
 app.get("/about",(req,res) => {
     res.send("there's nothing about me")
