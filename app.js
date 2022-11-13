b7b5fd8481d64b304de1ab5c965c73a10480a9c8
rcph <rcph110110@gmail.com>
2022-11-13 15:18:13 +0800

    newupdate

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const bodyParse = require('body-parser')
 const fs = require('fs')
 
-app.get("/",(req,res) => {
-    fs.readFileSync("./index.html","utf-8",(err,data) => {
-        res.send(`${data}`)
+app.use(bodyParse.urlencoded({ extended: true }))
+
+mongoose.connect(`${fs.readFileSync('.connection',"utf-8").split("<password>").join(fs.readFileSync(".env","utf-8"))}`)
+
+const Schema = {
+    text_note: String
+}
+const Note = mongoose.model("Note", Schema)
+
+const connection = mongoose.connection
+
+app.get("/", async (req, res) => {
+    res.write(fs.readFileSync("./index.html", "utf-8"))
+    await new Promise((resolved, rejected) => {
+        const collection = connection.db.collection("notes")
+        collection.find({}).toArray(function (err, data) {
+            data.forEach((item, index) => {
+                if (index == 0) res.write('<script async defer>')
+                res.write(`quick_note("${item.text_note}")\n`)
+                if (data.length - 1 == index) res.write(`</script>`)
+            })
+            resolved()
+        })
     })
-})
-app.get("/about",(req,res) => {
-    res.send("there's nothing about me")
+    res.end()
 })
 
-app.listen(3000)
+app.post("/", (req, res) => {
+    let note = new Note({
+        text_note: req.body.text_note
+    })
+    note.save()
+    res.redirect("/")
+})
+
+app.listen(fs.readFileSync('.port',"utf-8") || process.env.PORT, () => {
+    console.log("listening on port 3000")
+})
